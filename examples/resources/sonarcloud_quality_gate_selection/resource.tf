8960d54f8b4c9abfb471000b53b1fe9e8592f9bf
Arnav Bhutani <ArnavBhutani@lithia.com>
2022-08-01 18:19:58 -0700

    fixing data references

--- examples/resources/sonarcloud_quality_gate_selection/resource.tf
+++ examples/resources/sonarcloud_quality_gate_selection/resource.tf
@@ -5,6 +5,6 @@ data "sonarcloud_quality_gate" "awesome_qg" {
 data "sonarcloud_projects" "all" {}
 
 resource "sonarcloud_quality_gate_selection" "example_quality_gate_selection" {
-  gate_id   = awesome_qg.gate_id
-  selection = [for project in all : project.key if project.name == "My Awesome Project"]
+  gate_id   = data.sonarcloud_quality_gate.awesome_qg.gate_id
+  selection = [for project in data.sonarcloud_projects.all : project.key if project.name == "My Awesome Project"]
 }
