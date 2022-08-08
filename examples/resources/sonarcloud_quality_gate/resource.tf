a7e27a319a6706d029fb57cd55dc387999945010
Reinoud Kruithof <2184455+reinoudk@users.noreply.github.com>
2022-08-08 14:28:11 +0200

    docs: add docs for quality gates

--- examples/resources/sonarcloud_quality_gate/resource.tf
+++ examples/resources/sonarcloud_quality_gate/resource.tf
@@ -1,6 +1,6 @@
 resource "sonarcloud_quality_gate" "awesome" {
-  name      = "My Awesome Quality Gate"
-  isDefault = true
+  name       = "My Awesome Quality Gate"
+  is_default = true
   conditions = [
     // Less than 100% coverage
     {
--- examples/resources/sonarcloud_quality_gate_selection/resource.tf
+++ examples/resources/sonarcloud_quality_gate_selection/resource.tf
@@ -5,6 +5,6 @@ data "sonarcloud_quality_gate" "awesome_qg" {
 data "sonarcloud_projects" "all" {}
 
 resource "sonarcloud_quality_gate_selection" "example_quality_gate_selection" {
-  gate_id   = data.sonarcloud_quality_gate.awesome_qg.gate_id
+  gate_id      = data.sonarcloud_quality_gate.awesome_qg.gate_id
   project_keys = [for project in data.sonarcloud_projects.all : project.key if project.name == "My Awesome Project"]
 }
